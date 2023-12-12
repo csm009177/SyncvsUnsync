a4be14eaae859d025b4f9c8ce2196a511649cd3c
csm <csm009177@gmail.com>
2023-12-12 19:55:55 +0900

    callback with param

--- app.js
+++ app.js
@@ -2,13 +2,8 @@ function test(print){
   console.log(`result: ${print}`)
 }
 
-test("test");
-
-function later(callback){
-  const print = "test2"
-  callback(print);
+function later(pram, callback){
+  callback(pram)
 }
-
-later(test); // 콜백이면 안에서 부터 실행된다
-
+later("a", test)
 
