bfae6d6125c4c95efbe800282ecb6382876e1b17
kimwoojin123 <kimwoojin1903@gmail.com>
2023-12-18 16:47:21 +0900

    resolve reject

--- promise/promise-prac6.js
+++ promise/promise-prac6.js
@@ -1,13 +1,23 @@
-const changingStatePromise = new Promise((resolve) => {
+const changingStatePromise = new Promise((resolve, reject) => {
   console.log("Promise가 생성되었습니다.");
+  const condition = false;
+
   setTimeout(() => {
-    resolve("성공적으로 처리됨");
-    console.log("1초 후: Promise가 해결되었습니다.");
+    if (condition) {
+      resolve("성공적으로 처리됨");
+      console.log("1초 후: Promise가 해결되었습니다.");
+    } else {
+      reject("처리 실패");
+      console.log("1초 후 : Promise가 실패했습니다.");
+    }
   }, 1000);
   console.log("Promise가 생성된 직후에 실행됩니다.");
 });
 
-changingStatePromise.then((result) => {
-  console.log(result);
-  console.log("이 부분은 Promise가 해결된 후에 실행됩니다.");
-});
+changingStatePromise
+  .then((result) => {
+    console.log(result);
+  })
+  .catch((error) => {
+    console.log(error);
+  });
