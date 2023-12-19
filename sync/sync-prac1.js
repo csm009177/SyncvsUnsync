ebebe2109262fbed97636971a7bd81ba7c712c48
csm <csm009177@gmail.com>
2023-12-19 21:30:13 +0900

    sync-prac1.js is about Create a basic asynchronous function

--- sync/sync-prac1.js
+++ sync/sync-prac1.js
@@ -1,8 +1,6 @@
+// Step 1: 기본 비동기 함수 생성
 async function exampleAsyncFunction() {
     console.log("Step 1: Start");
     // 비동기 작업이 여기에 들어갈 것입니다.
-    console.log("sss")
     console.log("Step 2: End");
   }
-
-  exampleAsyncFunction()
