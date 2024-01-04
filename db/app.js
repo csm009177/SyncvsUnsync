43c9b216b5837861eb1ea818da9cb52e96ea4148
csm <csm009177@gmail.com>
2024-01-04 19:14:39 +0900

    add comment about xhr code

--- db/app.js
+++ db/app.js
@@ -28,10 +28,10 @@ const serv = http.createServer((req, res) => {
   // 요청이 GET 메서드이고 URL이 '/mariaDB'인 경우
   else if (req.method === "GET" && req.url === "/mariaDB") {
     // 'product' 테이블에서 모든 상품을 조회하는 SQL 쿼리를 작성합니다.
-    const query = "SELECT * FROM product";
+    const productQuery = "SELECT * FROM product"; // shop의 product table을 query에 담습니다
 
     // SQL 쿼리를 실행하여 상품 목록을 가져옵니다.
-    connection.query(query, (error, results) => {
+    connection.query(productQuery, (error, results) => {
       // 에러 발생 시 500 Internal Server Error 응답
       if (error) {
         res.writeHead(500, { "Content-Type": "application/json" });
